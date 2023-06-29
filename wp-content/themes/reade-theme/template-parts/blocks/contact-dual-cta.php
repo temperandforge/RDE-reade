b7cdb741f49bfe933ab81fd5f240dbe350251fbc
cwongtf <chris@temperandforge.com>
2023-06-29 08:07:40 -0400

    Contact page blocks

--- wp-content/themes/reade-theme/template-parts/blocks/contact-dual-cta.php
+++ wp-content/themes/reade-theme/template-parts/blocks/contact-dual-cta.php
@@ -6,3 +6,25 @@ echo '<script>console.log('.json_encode($fields, JSON_PRETTY_PRINT).');</script>
 }
 
 ?>
+
+<div class="contact-dual-cta--section">
+ <div class="contact-dual-cta--wrap">
+  <div class="contact-dual-cta--inner">
+   <?php foreach($fields['ctas'] as $index=>$cta) :?>
+    <div class="contact-dual-cta--single">
+     <?php if(!empty($cta['heading'])) :?>
+      <h2
+      class="<?php echo (empty($cta['content'])) ? 'heading-solo' : null ;?>"
+      ><?php echo $cta['heading'] ;?></h2>
+     <?php endif ;?>
+     <?php if(!empty($cta['content'])) :?>
+      <p><?php echo $cta['content'] ;?></p>
+     <?php endif ;?>
+     <?php if(!empty($cta['link'])) :?>
+      <a href="<?php echo $cta['link']['url'] ;?>" class="dual-cta-btn"><?php echo $cta['link']['title'] ;?></a>
+     <?php endif ;?>
+    </div>
+   <?php endforeach ;?>
+  </div>
+ </div>
+</div>
