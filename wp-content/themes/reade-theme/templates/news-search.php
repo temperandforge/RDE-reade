c1824974a04b3fb8de5b97629d00e66c5b8d23f5
sjmdotwork@gmail.com <scott@temperandforge.com>
2023-06-30 16:48:30 -0400

    qa fixes

--- wp-content/themes/reade-theme/templates/news-search.php
+++ wp-content/themes/reade-theme/templates/news-search.php
@@ -32,7 +32,7 @@ $view_more_button_text = get_field('view_more_button_text', 'options') ? get_fie
                 $hero_description = '(' . $search_results . ' ' . ($search_results == 1 ? 'result' : 'results') . ')';
                 $hero_search = false;
                 $hero_breadcrumbs = array(
-                    '/news/' => '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    '/news/' => '<svg class="first-arrow" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M7.99372 5.88128C7.65201 5.53957 7.09799 5.53957 6.75628 5.88128L3.25628 9.38128C2.91457 9.72299 2.91457 10.277 3.25628 10.6187L6.75628 14.1187C7.09799 14.4604 7.65201 14.4604 7.99372 14.1187C8.33543 13.777 8.33543 13.223 7.99372 12.8813L5.98744 10.875H16.125C16.6082 10.875 17 10.4832 17 10C17 9.51675 16.6082 9.125 16.125 9.125H5.98744L7.99372 7.11872C8.33543 6.77701 8.33543 6.22299 7.99372 5.88128Z" fill="#009FC6"/>
                     </svg> ' . ($options['back_to_all_news_button_text'] ? $options['back_to_all_news_button_text'] : 'Back To All News')
                 );
@@ -41,7 +41,7 @@ $view_more_button_text = get_field('view_more_button_text', 'options') ? get_fie
                 $hero_description = '&nbsp;';
                 $hero_search = false;
                 $hero_breadcrumbs = array(
-                    '/news/' => '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    '/news/' => '<svg class="first-arrow" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M7.99372 5.88128C7.65201 5.53957 7.09799 5.53957 6.75628 5.88128L3.25628 9.38128C2.91457 9.72299 2.91457 10.277 3.25628 10.6187L6.75628 14.1187C7.09799 14.4604 7.65201 14.4604 7.99372 14.1187C8.33543 13.777 8.33543 13.223 7.99372 12.8813L5.98744 10.875H16.125C16.6082 10.875 17 10.4832 17 10C17 9.51675 16.6082 9.125 16.125 9.125H5.98744L7.99372 7.11872C8.33543 6.77701 8.33543 6.22299 7.99372 5.88128Z" fill="#009FC6"/>
                     </svg> ' .( $options['back_to_all_news_button_text'] ? $options['back_to_all_news_button_text'] : 'Back To All News')
                 );
