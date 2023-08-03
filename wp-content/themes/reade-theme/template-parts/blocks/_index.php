4a2b5d105dd293e78404c782b770f14849e29e03
sjmdotwork@gmail.com <scott@temperandforge.com>
2023-08-03 13:12:20 -0400

    Update _index.php

--- wp-content/themes/reade-theme/template-parts/blocks/_index.php
+++ wp-content/themes/reade-theme/template-parts/blocks/_index.php
@@ -1115,29 +1115,6 @@ function theme_register_blocks()
       ),
    ]);
 
-   /** 
-    * Product RFQ Form
-    * */
-   acf_register_block([
-      'name'          => 'rfq-form',
-      'title'          => 'Product RFQ Form',
-      'render_template'   => get_stylesheet_directory() . "/template-parts/blocks/rfq-form.php",
-      'category'       => 'theme-blocks',
-      'icon'          => 'button',
-      // 'image'        => $img_root . '/primary-hero.webp',
-      'mode'          => $mode,
-      'keywords'       => ['rfq', 'form', 'reade', 'product', 'theme', TEXTDOMAIN],
-      'supports'      => ['align' => false],
-      'example'  => array(
-         'attributes' => array(
-            'mode' => 'preview',
-            'data' => array(
-               '_is_preview'   => 'true'
-            )
-         )
-      ),
-   ]);
-
 
          
 
