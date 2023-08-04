34928e2654e0391ff5e75f78b191d3607139b4d9
jgansl <jessegansler@gmail.com>
2023-08-04 08:28:51 -0700

    disable logging'

--- wp-content/themes/reade-theme/functions.php
+++ wp-content/themes/reade-theme/functions.php
@@ -3,7 +3,9 @@ define('TEXTDOMAIN', 'acl-theme');
 define("IS_LOCAL", wp_get_environment_type() == "local");
 define("REMOTE_URL", "https://reade.wpengine.com");
 
-ini_set("error_log", get_stylesheet_directory() . "/debug.txt");
+if( IS_LOCAL ) {
+   ini_set("error_log", get_stylesheet_directory() . "/debug.txt");
+}
 
 require_once( get_stylesheet_directory() . '/lib/theme-setup.php' );
 require_once( get_stylesheet_directory() . '/lib/theme-enqueue-scripts.php' );
