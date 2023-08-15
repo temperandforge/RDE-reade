d7885facc35b92f43f302c46db862bc8a7e1d595
sjmdotwork@gmail.com <scott@temperandforge.com>
2023-08-15 15:14:03 -0400

    QA and aouth start

--- wp-content/themes/reade-theme/functions.php
+++ wp-content/themes/reade-theme/functions.php
@@ -54,3 +54,150 @@ add_filter( 'woocommerce_taxonomy_args_product_tag', 'enable_taxonomy_rest' );
 add_filter('woocommerce_resize_images', static function() {
     return false;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Salesforce
+ */
+add_menu_page(
+   'Salesforce OAuth', // name in title bar
+   'Salesforce OAuth', // text in menu
+   'activate_plugins', // capability required - should be adminstrator capability
+   'salesforce-oauth',  // menu slug
+   'doSalesforceOAuth', // callback function for content
+   'dashicons-money-alt', // icon
+   '999'                  // position - we'll put it at the bottom
+);
+
+function getAccessToken($code) {
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://test.salesforce.com/services/oauth2/token',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST  => "POST",
+        CURLOPT_POSTFIELDS => 
+            array(
+                'grant_type' => 'authorization_code',
+                'client_id' => '3MVG9eMnfmfDO5NCm2GJ.ybXbGH8w.AT81nrmhmZ2YuPyf9wi1RdfBCwryqA9sigaG26HlP1hEBPMy5xcqEtU',
+                'client_secret' => '780E2DBC70E8BA933AE11F149986C4D92D8258EBD6B8ED7BDE1073D560ABDF00',
+                'redirect_uri' => get_site_url() . '/wp-admin/admin.php?page=salesforce-oauth',
+                'code' => $code
+            )
+        ,
+        CURLOPT_HTTPHEADER     => array(
+            "application/x-www-form-urlencoded"
+        )
+    ));
+
+    $response = curl_exec( $curl );
+    $err      = curl_error( $curl );
+    curl_close( $curl );
+    return $response;
+
+}
+
+function refreshToken($token) {
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://test.salesforce.com/services/oauth2/token',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST  => "POST",
+        CURLOPT_POSTFIELDS => 
+            array(
+                'grant_type' => 'refresh_token',
+                'client_id' => '3MVG9eMnfmfDO5NCm2GJ.ybXbGH8w.AT81nrmhmZ2YuPyf9wi1RdfBCwryqA9sigaG26HlP1hEBPMy5xcqEtU',
+                'client_secret' => '780E2DBC70E8BA933AE11F149986C4D92D8258EBD6B8ED7BDE1073D560ABDF00',
+                'refresh_token' => $token
+            )
+        ,
+        CURLOPT_HTTPHEADER     => array(
+            "application/x-www-form-urlencoded"
+        )
+    ));
+
+    $response = curl_exec( $curl );
+    $err      = curl_error( $curl );
+    curl_close( $curl );
+    return $response;
+}
+
+
+
+function doSalesforceOAuth() {
+
+   // get tokens
+   $accessToken = get_option('salesforce_access_token');
+   $refreshToken = get_option('salesforce_refresh_token');
+   
+   // check for code parameter in url
+   if (!empty($_GET['code'])) {
+
+      $token = json_decode(getAccessToken($_GET['code']));
+      
+
+      update_option('salesforce_access_token', $token->access_token);
+      update_option('salesforce_refresh_token', $token->refresh_token);
+
+      ?>
+      <div class="salesforce-oauth-successful" style="padding-top: 50px;">
+         <h1>Salesforce Authorized</h1>
+         <p>Salesforce has been successfully connected and authorized.</p>
+      </div>
+      <?php
+
+
+   } else {
+
+      if (!empty($accessToken) && !empty($refreshToken)) {
+
+         
+         $new = json_decode(refreshToken($refreshToken));
+         update_option('salesforce_access_token', $new->access_token);
+         //update_option('salesforce_refresh_token', $new->refresh_token);
+
+         //echo '<pre>'; print_r($new); echo '</pre>';
+
+         //echo get_option('salesforce_access_token');
+         //echo get_option('salesforce_refresh_token');
+         
+         ?>
+         <div class="salesforce-oauth-successful" style="padding-top: 50px;">
+            <h1>Salesforce Authorized</h1>
+            <p>Salesforce is authorized.  Nothing to do here.</p>
+         </div>
+         <?php
+      } else {
+
+         // admin needs to authorize
+         ?>
+         <div class="salesforce-oauth" style="padding-top: 50px;">
+            <h1>Connect To Salesforce</h1>
+            <p>Salesforce app is not connected and authorized.  Click the link below to authorize this website's connection to salesforce.</p>
+            <a href="https://test.salesforce.com/services/oauth2/authorize?response_type=code&client_id=3MVG9eMnfmfDO5NCm2GJ.ybXbGH8w.AT81nrmhmZ2YuPyf9wi1RdfBCwryqA9sigaG26HlP1hEBPMy5xcqEtU&redirect_uri=<?php echo get_site_url(); ?>/wp-admin/admin.php?page=salesforce-oauth">Authorize Salesforce</a>
+         </div>
+         <?php
+      }
+   }
+}
