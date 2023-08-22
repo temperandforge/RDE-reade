4b279d1d6a70117d2763bd260f69326d1f67f885
cwongtf <chris@temperandforge.com>
2023-08-22 14:19:01 -0400

    History Block QA

--- wp-content/themes/reade-theme/template-parts/history/2018.php
+++ wp-content/themes/reade-theme/template-parts/history/2018.php
@@ -4,14 +4,9 @@
       <div class="max-w-md"><?php echo __($args['content'], TEXTDOMAIN);?></div>
    </div>
    <?php if(!empty($args['image'])) :?>
-      <figure class="primary-figure">
+      <figure>
          <?php echo wp_get_attachment_image($args['image']['id'], 'full', ) ;?>
       </figure>
-   <?php if(!empty($args['secondary_image'])) :?>
-      <figure class="secondary-figure">
-         <?php echo wp_get_attachment_image($args['secondary_image']['id'], 'full', ) ;?>
-      </figure>
-   <?php endif ;?>
    <?php endif ;?>
    <h3 class="mt-4"><?php echo __($args['heading'], TEXTDOMAIN);?></h3>
 </div>
--- wp-content/themes/reade-theme/template-parts/history/2020.php
+++ wp-content/themes/reade-theme/template-parts/history/2020.php
@@ -4,4 +4,9 @@
   <h3 class=""><?php echo __($args['heading'], TEXTDOMAIN);?></h3>  
   <div class="history-content--info"><?php echo __($args['content'], TEXTDOMAIN);?></div>
  </div>
+ <?php if(!empty($args['image'])) :?>
+   <figure>
+      <?php echo wp_get_attachment_image($args['image']['id'], 'full', ) ;?>
+   </figure>
+ <?php endif ;?>
 </div>
