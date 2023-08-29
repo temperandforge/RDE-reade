41b9345627c26669bcb886955bf3599a647abda9
sjmdotwork@gmail.com <scott@temperandforge.com>
2023-08-29 10:36:00 -0400

    turn off debugging after turning it on

--- wp-content/themes/reade-theme/functions.php
+++ wp-content/themes/reade-theme/functions.php
@@ -3,9 +3,9 @@ define('TEXTDOMAIN', 'acl-theme');
 define("IS_LOCAL", wp_get_environment_type() == "local");
 define("REMOTE_URL", "https://reade.wpengine.com");
 
-//if( IS_LOCAL ) {
+if( IS_LOCAL ) {
    ini_set("error_log", get_stylesheet_directory() . "/debug.txt");
-//}
+}
 
 require_once( get_stylesheet_directory() . '/lib/theme-setup.php' );
 require_once( get_stylesheet_directory() . '/lib/theme-enqueue-scripts.php' );
