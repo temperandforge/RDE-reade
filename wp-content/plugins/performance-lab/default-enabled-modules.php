c5a326c5031958bb8cd13dd2bd640edb9d22bde7
jgansl <jessegansler@gmail.com>
2023-10-02 11:07:20 -0700

    updated README; apply gitignore

--- wp-content/plugins/performance-lab/default-enabled-modules.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
-return array(
-	'images/dominant-color-images',
-	'images/fetchpriority',
-	'images/webp-support',
-	'images/webp-uploads',
-);
-/* THIS IS THE END OF THE GENERATED FILE */
--- wp-content/plugins/wp-sync-db-master/module-cli/version.php
+++
@@ -1,2 +0,0 @@
-<?php
-$GLOBALS['wpsdb_meta']['wp-sync-db-cli']['version'] = '1.0b1';
