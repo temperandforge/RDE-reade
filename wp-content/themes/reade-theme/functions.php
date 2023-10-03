f1a29f08aafffefd450d1afeb79eef89bdccc37d
sjmdotwork@gmail.com <scott@temperandforge.com>
2023-10-03 15:27:39 -0400

    remove tfdb-sync

--- wp-content/themes/reade-theme/functions.php
+++ wp-content/themes/reade-theme/functions.php
@@ -11,9 +11,6 @@ require_once( get_stylesheet_directory() . '/lib/theme-setup.php' );
 require_once( get_stylesheet_directory() . '/lib/theme-enqueue-scripts.php' );
 require_once( get_stylesheet_directory() . '/template-parts/blocks/_index.php' );
 
-//PRE_LAUNCH
-require_once (get_stylesheet_directory() . '/lib/tf-db-sync.php');
-
 /** Language Switching -> handled by gtranslate */
 // add_action( 'after_setup_theme', 'reade_load_theme_textdomain' );
 // function reade_load_theme_textdomain() {
