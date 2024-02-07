dad3637f415ba0f778dbacce54080b96c316d6a6
scottmartindevelopment <scottmartindevelopment@gmail.com>
2024-02-07 15:34:35 -0500

    Update index.js

--- wp-content/themes/reade-theme/src/index.js
+++ wp-content/themes/reade-theme/src/index.js
@@ -1,15 +1,15 @@
 // match unique body_class
 import Router from './util/Router'
 import common from './routes/common'
-// import customProductRfqForm from './routes/custom-product-rfq-form.js'
-// import pageTemplateLegal from './routes/legal'
-// import single from './routes/single'
-// import frontPage from './routes/front-page' //SETUP
+import customProductRfqForm from './routes/custom-product-rfq-form.js'
+import pageTemplateLegal from './routes/legal'
+import single from './routes/single'
+import frontPage from './routes/front-page' //SETUP
 import history from './routes/history-new';
-// import singleProduct from './routes/single-product';
-// import itemizedRfq from './routes/itemized-rfq';
-// import pageId103 from './routes/particle-measurement';
-// import search from './routes/search';
+import singleProduct from './routes/single-product';
+import itemizedRfq from './routes/itemized-rfq';
+import pageId103 from './routes/particle-measurement';
+import search from './routes/search';
 
 /**
  * Populate Router instance with DOM routes
@@ -17,15 +17,15 @@ import history from './routes/history-new';
  */
 const routes = new Router({
   common,
-  // customProductRfqForm,
-  // single,
-  // pageTemplateLegal,
-  // frontPage,
+   customProductRfqForm,
+   single,
+   pageTemplateLegal,
+   frontPage,
   history,
-  // singleProduct,
-  // itemizedRfq,
-  // pageId103,
-  // search
+   singleProduct,
+   itemizedRfq,
+   pageId103,
+   search
 })
 
 /** Load Events */
