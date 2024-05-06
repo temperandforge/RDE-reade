ee2075153d0c51aaa0b156954d89ae96144434be
jgansl <jessegansler@gmail.com>
2024-05-06 12:28:46 -0700

    change secondary primary hero banner button to white-blue theme

--- wp-content/themes/reade-theme/template-parts/blocks/primary-hero-banner.php
+++ wp-content/themes/reade-theme/template-parts/blocks/primary-hero-banner.php
@@ -29,7 +29,7 @@ if (!empty($block['data']['_is_preview'])) {
                         continue;
                      }
                   ?>
-                     <a class="btn-blue-dark-blue btn-arrow" href="<?php echo $btn['url']; ?>" target="<?php echo $btn['target'] ?: '_self'; ?>">
+                     <a class="<?php echo $idx == 0 ? "btn-blue-dark-blue" : "btn-white-blue";?> btn-arrow" href="<?php echo $btn['url']; ?>" target="<?php echo $btn['target'] ?: '_self'; ?>">
                         <span><?php echo $btn['title']; ?></span>
                         <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                            <path fill-rule="evenodd" clip-rule="evenodd" d="M11.1151 5.22214C11.5708 4.76653 12.3095 4.76653 12.7651 5.22214L17.4317 9.88881C17.8873 10.3444 17.8873 11.0831 17.4317 11.5387L12.7651 16.2054C12.3095 16.661 11.5708 16.661 11.1151 16.2054C10.6595 15.7498 10.6595 15.0111 11.1151 14.5555L13.7902 11.8804L4.94011 11.8804C4.29577 11.8804 3.77344 11.3581 3.77344 10.7138C3.77344 10.0694 4.29577 9.5471 4.94011 9.5471H13.7902L11.1151 6.87206C10.6595 6.41645 10.6595 5.67775 11.1151 5.22214Z" fill="white" />
