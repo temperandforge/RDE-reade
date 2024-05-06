658d72899f2f01670527a59d0c0101759cc28606
jgansl <jessegansler@gmail.com>
2024-05-06 12:02:36 -0700

    primary hero multiple buttons functionality

--- wp-content/themes/reade-theme/template-parts/blocks/primary-hero-banner.php
+++ wp-content/themes/reade-theme/template-parts/blocks/primary-hero-banner.php
@@ -20,14 +20,32 @@ if (!empty($block['data']['_is_preview'])) {
                <?php if (!empty($fields['content'])) : ?>
                   <p><?php echo $fields['content']; ?></p>
                <?php endif; ?>
-               <?php if (!empty($fields['link'])) : ?>
+               <?php 
+               if ( array_key_exists('buttons', $fields) && $buttons = $fields['buttons']) {  ?>
+               <div class="buttons--wrap">
+               <?php
+                  foreach($buttons as $idx => $button) :
+                     if( ! $btn = $button['btn'] ) {
+                        continue;
+                     }
+                  ?>
+                     <a class="btn-blue-dark-blue btn-arrow" href="<?php echo $btn['url']; ?>" target="<?php echo $btn['target'] ?: '_self'; ?>">
+                        <span><?php echo $btn['title']; ?></span>
+                        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                           <path fill-rule="evenodd" clip-rule="evenodd" d="M11.1151 5.22214C11.5708 4.76653 12.3095 4.76653 12.7651 5.22214L17.4317 9.88881C17.8873 10.3444 17.8873 11.0831 17.4317 11.5387L12.7651 16.2054C12.3095 16.661 11.5708 16.661 11.1151 16.2054C10.6595 15.7498 10.6595 15.0111 11.1151 14.5555L13.7902 11.8804L4.94011 11.8804C4.29577 11.8804 3.77344 11.3581 3.77344 10.7138C3.77344 10.0694 4.29577 9.5471 4.94011 9.5471H13.7902L11.1151 6.87206C10.6595 6.41645 10.6595 5.67775 11.1151 5.22214Z" fill="white" />
+                        </svg>
+                     </a>
+                  <?php endforeach; ?>
+               </div>
+               <?php 
+               } else if (!empty($fields['link'])) { ?>
                   <a class="btn-blue-dark-blue btn-arrow" href="<?php echo $fields['link']['url']; ?>" target="<?php echo $fields['link']['target'] ?: '_self'; ?>">
                      <span><?php echo $fields['link']['title']; ?></span>
                      <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M11.1151 5.22214C11.5708 4.76653 12.3095 4.76653 12.7651 5.22214L17.4317 9.88881C17.8873 10.3444 17.8873 11.0831 17.4317 11.5387L12.7651 16.2054C12.3095 16.661 11.5708 16.661 11.1151 16.2054C10.6595 15.7498 10.6595 15.0111 11.1151 14.5555L13.7902 11.8804L4.94011 11.8804C4.29577 11.8804 3.77344 11.3581 3.77344 10.7138C3.77344 10.0694 4.29577 9.5471 4.94011 9.5471H13.7902L11.1151 6.87206C10.6595 6.41645 10.6595 5.67775 11.1151 5.22214Z" fill="white" />
                      </svg>
                   </a>
-               <?php endif; ?>
+               <?php } ?>
                <div class="primary-hero--decor" aria-hidden="true">
                   <svg class="small-boxes" width="138" height="176" viewBox="0 0 138 176" fill="none" xmlns="http://www.w3.org/2000/svg">
                      <path d="M136.844 27.6575L91.5328 1L46.2812 27.6575L91.5583 54.3149V113.099L136.844 86.4497V27.6575ZM136.844 27.6575L91.5583 54.3149L46.2812 27.6575V86.4497L91.5583 113.099" stroke="#AEE3F0" stroke-width="2" stroke-linejoin="round" />
