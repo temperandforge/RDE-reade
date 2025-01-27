f660e5898dbc4d92fc6ab30c91135d7368d5388b
Madison Smith <madisonsmith@Madisons-MacBook-Pro.local>
2025-01-27 10:30:50 -0500

    added if statement around the product cta

--- wp-content/themes/reade-theme/template-parts/blocks/product-call-to-action.php
+++ wp-content/themes/reade-theme/template-parts/blocks/product-call-to-action.php
@@ -11,22 +11,24 @@ if( !empty( $block['data']['_is_preview'] ) ) {
 } else if( $fields = $args) {
 ?>
 
+<?php if( $fields['cta_copy-row'] ): ?>
 
+   <section class="product-cta"
+   style="
+         <?php if( $pt = $fields['padding-row']['padding-top'] ): ?>
+            padding-top: <?php echo $fields['padding-row']['padding-top'].'em;'; ?>
+         <?php endif; ?>
+         <?php if( $pb = $fields['padding-row']['padding-bottom'] ): ?>
+            padding-bottom: <?php echo $fields['padding-row']['padding-bottom'].'em;'; ?>
+         <?php endif; ?>
+      ">
+      <div class="product-cta--wrap">
+         <h4><?php echo $fields['cta_copy']; ?></h4>
+         <a class="btn-blue-dark-blue" href="<?php echo $fields['cta_link']['url']; ?>"><?php echo $fields['cta_link']['title']; ?></a>
+      </div>
+   </section>
 
-<section class="product-cta"
-style="
-      <?php if( $pt = $fields['padding-row']['padding-top'] ): ?>
-         padding-top: <?php echo $fields['padding-row']['padding-top'].'em;'; ?>
-      <?php endif; ?>
-      <?php if( $pb = $fields['padding-row']['padding-bottom'] ): ?>
-         padding-bottom: <?php echo $fields['padding-row']['padding-bottom'].'em;'; ?>
-      <?php endif; ?>
-   ">
-   <div class="product-cta--wrap">
-      <h4><?php echo $fields['cta_copy']; ?></h4>
-      <a class="btn-blue-dark-blue" href="<?php echo $fields['cta_link']['url']; ?>"><?php echo $fields['cta_link']['title']; ?></a>
-   </div>
-</section>
+<?php endif; ?>
 
 
 <?php 
